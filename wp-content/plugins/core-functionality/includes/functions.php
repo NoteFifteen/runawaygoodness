3ba0dd7f7773e5030a531aae57aa5994547abe1c
John Hawkins <john@9seeds.com>
2015-08-24 10:14:16 -0700

    Clean up text links, widgets for social

--- wp-content/plugins/core-functionality/includes/functions.php
+++ wp-content/plugins/core-functionality/includes/functions.php
@@ -144,3 +144,18 @@ function rg_deal_columns( $column, $post_id ) {
 			
 	}
 }
+
+add_action( 'genesis_before', 'rg_facebook_code' );
+
+function rg_facebook_code() {
+?>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=341168189960";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+<?php
+}
