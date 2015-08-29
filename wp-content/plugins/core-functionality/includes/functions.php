3788a1d2e3d4630a32d7a076c2ae1df0125b4634
John Hawkins <john@9seeds.com>
2015-08-29 14:56:45 -0700

    hide admin bar for logged in users

--- wp-content/plugins/core-functionality/includes/functions.php
+++ wp-content/plugins/core-functionality/includes/functions.php
@@ -162,8 +162,12 @@ function rg_facebook_code() {
 
 add_action('after_setup_theme', 'remove_admin_bar');
 
+
+/*
+Remove admin bar
+ */
 function remove_admin_bar() {
-if (!current_user_can('administrator') && !is_admin()) {
-  show_admin_bar(false);
-}
+	if (!current_user_can('administrator') && !is_admin()) {
+		show_admin_bar(false);
+	}
 }
