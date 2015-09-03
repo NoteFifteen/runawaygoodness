555e9196298dbfee807b5344f89e5db7b06b8258
John Hawkins <john@9seeds.com>
2015-09-02 23:37:42 -0700

    Add taxonomy for genres

--- wp-content/plugins/core-functionality/includes/functions.php
+++ wp-content/plugins/core-functionality/includes/functions.php
@@ -171,3 +171,38 @@ function remove_admin_bar() {
 		show_admin_bar(false);
 	}
 }
+
+add_action( 'init', 'create_genre_taxonomy', 0 );
+
+function create_genre_taxonomy() {
+
+	$labels = array(
+	'name' => _x( 'Genres', 'taxonomy general name' ),
+	'singular_name' => _x( 'Genre', 'taxonomy singular name' ),
+	'search_items' =>  __( 'Search Genres' ),
+	'popular_items' => __( 'Popular Genres' ),
+	'all_items' => __( 'All Genres' ),
+	'parent_item' => null,
+	'parent_item_colon' => null,
+	'edit_item' => __( 'Edit Topic' ), 
+	'update_item' => __( 'Update Topic' ),
+	'add_new_item' => __( 'Add New Topic' ),
+	'new_item_name' => __( 'New Topic Name' ),
+	'separate_items_with_commas' => __( 'Separate Genres with commas' ),
+	'add_or_remove_items' => __( 'Add or remove Genres' ),
+	'choose_from_most_used' => __( 'Choose from the most used Genres' ),
+	'menu_name' => __( 'Genres' ),
+	); 
+
+// Now register the non-hierarchical taxonomy like tag
+
+  register_taxonomy( 'deal_genres','deal',array(
+'hierarchical' => false,
+'labels' => $labels,
+'show_ui' => true,
+'show_admin_column' => true,
+'update_count_callback' => '_update_post_term_count',
+'query_var' => true,
+'rewrite' => array( 'slug' => 'genres' ),
+  ));
+}
