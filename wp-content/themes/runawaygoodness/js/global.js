b0ede357e8cf9892f5691f4b461d9a81522e07b6
John Hawkins <john@9seeds.com>
2015-09-23 15:18:49 -0700

    cleaning up the header CSS

--- wp-content/themes/runawaygoodness/js/global.js
+++ wp-content/themes/runawaygoodness/js/global.js
@@ -1,14 +1,6 @@
 jQuery(function( $ ){
 
 	$(".site-header").after('<div class="bumper"></div>');
-
-	$(window).scroll(function () {
-	  if ($(document).scrollTop() > 1 ) {
-	    $('.site-header').addClass('shrink');
-	  } else {
-	    $('.site-header').removeClass('shrink');
-	  }
-	});
     
     $("header .genesis-nav-menu").addClass("responsive-menu").before('<div id="responsive-menu-icon"></div>');
     
