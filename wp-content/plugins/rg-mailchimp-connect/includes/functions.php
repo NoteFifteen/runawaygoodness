5863e5a1a20a5895da8ac8c9eb5fb7ab555d686a
Brian Ronald <brronald@gmail.com>
2015-11-18 11:39:43 -0800

    MailChimp Connect Tweaks
    I made some changes here based on observations I saw during our big RG campaign.
    
    We hit the MC API pretty hard to pull the Interests/Categories (Genres) lists, so
    I added object-caching around it.  With a stock WP install, this will at least
    cache during the page generation, but with back-end cache configured, it will
    now cache for much longer.  I've added the 'memcached-is-your-friend' plugin which
    adds the memcache or memcached extension as a WP Object Cache provider.  It's very
    simple, and in testing seems to work wonderfully.  This should improve our page load
    times a bit and reduce the amount of calls we make to MailChimp.
    
    I made a few bug fixes such as needing to add quotes around the RG_INCLUDE_PATH define
    which threw warnings, and adding a quote onto the end of a page redirect to the Thank
    You Page after updating a subscription.
    
    I also made a tweak to make it easier to update the MailChimp API URL if needed by
    defining it up at the top of the file.
    
    I also changed the way we generate our JSON requests by using standard PHP objects
    and then json_encode()'ing them, which is easier to code around and reduces the chances
    of errors creating JSON by hand.

--- wp-content/plugins/rg-mailchimp-connect/includes/functions.php
+++ wp-content/plugins/rg-mailchimp-connect/includes/functions.php
@@ -1,4 +1,9 @@
 <?php
+
+/**
+ * MailChimp Base URL for API
+ */
+define( 'RG_MC_BASE_URL', 'http://us11.api.mailchimp.com/3.0/lists/' );
 // sign up mailchimp shortcode
 // 
 // To display content on sign up page, add [rgmcsignup]
@@ -8,14 +13,13 @@ function rg_signup_form( $hidegenre = false, $button_text ) {
 		$_POST["lp-source"] = esc_attr( $_GET["ref"] );
 	}
 
-	// get genres (interest categories)
-		$url = 'http://us11.api.mailchimp.com/3.0/lists/' . LIST_ID . '/interest-categories/' . INTEREST_TYPE . '/interests?count=100&output=json';
-    	$response = \Httpful\Request::get($url)
-        ->authenticateWith(API_KEY, API_KEY)        // authenticate with basic auth...
-        ->send();
-	$genre_options = '';
+	// Grab the Interest Categories from MC
+	$interest_categories = rg_get_interest_categories();
 
-	foreach ($response->body->interests as $genre) {
+	$genre_options = '';
+	$showhiddenc = '';
+
+	foreach ($interest_categories as $genre) {
 		if( isset( $_GET["c"] ) && ( $_GET["c"] == $genre->id ) ) {
 			$preselect = "selected";
 			$showhiddenc = '<input type="hidden" name="lp-genre" value="' . $genre->id . ':' . $genre->name . '">';
@@ -68,33 +72,36 @@ function process_rg_signup() {
     // if the submit button is clicked, send the email
     if ( isset( $_POST['lp-submitted'] ) ) {
 
-    	$emailmd5   = md5(sanitize_email( $_POST["lp-email"] ));
         // sanitize form values
         $email   = sanitize_email( $_POST["lp-email"] );
-        $genre = sanitize_text_field( $_POST["lp-genre"] );
         $genre = explode(":", sanitize_text_field( $_POST["lp-genre"] ));
 		$genre_code = $genre[0]; // category interest for the genre
 		$genre_name = $genre[1]; // text name of tbe genre
         $source = sanitize_text_field( $_POST["lp-source"] );
 
 		// get genres (interest categories)
-		$url = 'http://us11.api.mailchimp.com/3.0/lists/' . LIST_ID . '/members/';
+		$url = RG_MC_BASE_URL . LIST_ID . '/members/';
 
 		if( isset( $_POST["lp-genre"] ) ) {
+
+			// I prefer this approach because it's easier to work with
+			// and you don't have to worry about manually building it as JSON
+			$request = new stdClass();
+			$request->email_address = $email;
+			$request->status = 'subscribed';
+			$request->merge_fields = [
+				'SOURCE' => $source,
+				'FIRSTCAT' => $genre_name
+			];
+			$request->interests = [
+				$genre_code => true
+			];
+
 			// has genres
 			$response = \Httpful\Request::post($url)		// Build a POST request...
 			    ->sendsJson()								// tell it we're sending (Content-Type) JSON...
 			    ->authenticateWith(API_KEY, API_KEY)		// authenticate with basic auth...
-			    ->body('{	"email_address": "' . $email . '", 
-	    					"status": "subscribed", 
-	    					"merge_fields": {
-	    						"SOURCE": "' . $source . '",
-	    						"FIRSTCAT": "' . $genre_name . '"
-	    					},
-	    					"interests": {
-	  						    "' . $genre_code . '": true
-	    					}
-						}')             					// attach a body/payload...
+				->body(json_encode($request))
 			    ->send();                      				// and finally, fire that thing off!
 			    add_post_meta( 1, 'rgmcresponse', $response, false );
 		} else {
@@ -194,16 +201,18 @@ function process_rg_signup() {
 
 			}
 
+			$request = new stdClass();
+			$request->email_address = $email;
+			$request->status = 'subscribed';
+			$request->merge_fields = [
+					'SOURCE' => $source,
+					'FIRSTCAT' => $first_cat
+			];
+
 			$response = \Httpful\Request::post($url)		// Build a POST request...
 			    ->sendsJson()								// tell it we're sending (Content-Type) JSON...
 			    ->authenticateWith(API_KEY, API_KEY)		// authenticate with basic auth...
-			    ->body('{	"email_address": "' . $email . '", 
-	    					"status": "subscribed", 
-	    					"merge_fields": {
-	    						"SOURCE": "' . $source . '",
-	    						"FIRSTCAT": "'. $first_cat .'"
-	    					}
-						}')             					// attach a body/payload...
+			    ->body(json_encode($request))             					// attach a body/payload...
 			    ->send();                      				// and finally, fire that thing off!
 			    add_post_meta( 1, 'rgmcresponse', $response, false );
 
@@ -245,6 +254,8 @@ add_shortcode( 'rgmcsignup' , 'rg_mailchimp_sign_up' );
 // 
 function rg_mailchimp_genres_form() {
 
+	$html = '';
+
     if ( isset( $_POST['lp-submitted'] ) ) {
     	$emailmd5 = md5( strtolower( sanitize_email( $_POST["lp-email"] )));
     	$email  = strtolower( sanitize_email( $_POST["lp-email"] ));
@@ -279,27 +290,26 @@ function rg_mailchimp_genres_form() {
    		$html .= '</form>';
    	} else {
 
-	   	// grab list of genres
-		$url = 'http://us11.api.mailchimp.com/3.0/lists/' . LIST_ID . '/interest-categories/' . INTEREST_TYPE . '/interests/?count=100&output=json';
-		$response = \Httpful\Request::get($url)
-        ->authenticateWith(API_KEY, API_KEY)        // authenticate with basic auth...
-        ->send();
-
-		// grab member data
-		$url2 = 'http://us11.api.mailchimp.com/3.0/lists/' . LIST_ID . '/members/'. $emailmd5 .'?output=json';
+	    // grab member data
+		$url2 = RG_MC_BASE_URL . LIST_ID . '/members/'. $emailmd5 .'?output=json';
 		$response2 = \Httpful\Request::get($url2)
         ->authenticateWith(API_KEY, API_KEY)        // authenticate with basic auth...
         ->send();
 
 		$interest_array = array();
-		foreach( $response2->body->interests as $k=>$v ) {
-			if( $v == 1 ) {
-				$interest_array[] = $k;
-			} 
-		}
+
+	    // Oddly, we've received responses with no interests or body and PHP spits out a warning
+	    if($response2->body && $response2->body->interests) {
+		    foreach( $response2->body->interests as $k => $v ) {
+			    if( $v == 1 ) {
+				    $interest_array[] = $k;
+			    }
+		    }
+	    }
 
 		global $post;
 		$slug = get_post( $post )->post_name;
+	    $output = '';
 
 		if ( isset($_GET['st'] ) && $_GET['st'] == 'up' ) {
 			$output = "<p><strong>Your settings have been saved!</strong></p>"; 
@@ -308,7 +318,10 @@ function rg_mailchimp_genres_form() {
 		$output .= '<form method="post">
 	        <fieldset>';
 
-		foreach ($response->body->interests as $genre) {
+	    // Grab the Interest Categories from MC
+	    $interest_categories = rg_get_interest_categories();
+
+	    foreach ($interest_categories as $genre) {
 			if( in_array( $genre->id, $interest_array ) ) {
 				$checkinterest = 'checked';
 			} else {
@@ -330,53 +343,45 @@ function rg_mailchimp_genres_form() {
 function process_rg_genres() {
 
 	// grab entire list of interests
-	$interest_url = 'http://us11.api.mailchimp.com/3.0/lists/' . LIST_ID . '/interest-categories/' . INTEREST_TYPE . '/interests?count=100&output=json';
-		$interest_response = \Httpful\Request::get($interest_url)
-        ->authenticateWith(API_KEY, API_KEY)        // authenticate with basic auth...
-        ->send();
-
+	$interest_categories = rg_get_interest_categories();
 	$full_interest_list = array();
 
-	foreach ($interest_response->body->interests as $interests) {
+	foreach ($interest_categories as $interests) {
 		$full_interest_list[] = $interests->id;
 	}
 
-   // if the submit button is clicked, send the email
-    if ( isset( $_POST['lp-genres-submitted'] ) ) {
+	// if the submit button is clicked, send the email
+	if ( isset( $_POST['lp-genres-submitted'] ) ) {
     	$email = $_POST["lp-email"];
-    	$mailmd5 = md5($email);
-        $genres = $_POST["lp-genres"];
 
-        $first = true;
-		foreach ($genres as $genre) {
-			if ( $first ) {
-				// no comma
-				$first = false;
-			} else {
-				$genre_list .= ',';
-			}
+		// If they didn't set any genres, we need to create an empty array
+        $genres = (isset($_POST["lp-genres"])) ? $_POST["lp-genres"] : [];
 
-			$genre_list .= '"' . $genre . '":true';
-		}
+	    // Build the update request
+	    $request = new stdClass();
+	    $request->email_address = $email;
+	    $request->interests = [];
 
-		// add unchecked genres to the list
+	    // Add their selected genres
+	    foreach($genres as $genre) {
+		    $request->interests[$genre] = true;
+	    }
+
+		// Add unchecked genres to the list
 		$not_checked = array_diff( $full_interest_list, $genres );
-
 		foreach ($not_checked as $nci ) {
-			$genre_list .= ',"' . $nci . '":false';
+			$request->interests[$nci] = false;
 		}
 
-  		$url = 'http://us11.api.mailchimp.com/3.0/lists/' . LIST_ID . '/members/' . $mailmd5;
-		$response = \Httpful\Request::patch($url)        // Build a POST request to update existing user
+  		$url = sprintf(RG_MC_BASE_URL . LIST_ID . '/members/%s', md5($email));
+		$response = \Httpful\Request::patch($url)       // Build a POST request to update existing user
 		    ->sendsJson()                             	// tell it we're sending (Content-Type) JSON...
-		    ->authenticateWith(API_KEY, API_KEY)  // authenticate with basic auth...
-		    ->body('{	
-		    			"email_address": "'. $email . '",
-    					"interests":{' . $genre_list . '}
-    				}')             					// attach a body/payload...
+		    ->authenticateWith(API_KEY, API_KEY)        // authenticate with basic auth...
+		    ->body(json_encode($request))				// attach a body/payload...
 		    ->send();                      				// and finally, fire that thing off!
  
  		if ($response->body->status == "404") {
+		    error_log(sprintf('Unable to locate user %s in Mailchimp!', $email));
  			echo  '<div class="genreerror"><strong>** ' . $response->body->status . ' ** Hmmm, something Strange happened. We could not locate this email in the system. Please contact books@runawaygoodness.com and we will be glad to help! **</strong></div>';
 	    	rg_mailchimp_genres_form();
  		} else {
@@ -384,7 +389,7 @@ function process_rg_genres() {
  			// redirect based on where the visitor came from
  			if( $_POST["frompage"] == get_post( get_option( 'almost_done_page' ) )->post_name ) {
 	 			echo '<script type="text/javascript">';
-	    		echo 'window.location.href = "' . get_site_url() . '/' . get_page_uri( get_option( 'thank_you_page' ) ) . "/";
+	    		echo 'window.location.href = "' . get_site_url() . '/' . get_page_uri( get_option( 'thank_you_page' ) ) . '/"';
 				echo '</script>';
 			} else {
 	 			echo '<script type="text/javascript">';
@@ -407,6 +412,30 @@ function rg_mailchimp_genres() {
 
 add_shortcode( 'rgmcthankyou' , 'rg_mailchimp_genres' );
 
+/**
+ * Function to grab the Interest Categories from MC, utilizing cache if possible
+ */
+function rg_get_interest_categories()
+{
+	if(! $interests = wp_cache_get('rg_interests', 'rg')) {
+
+		$interest_url = RG_MC_BASE_URL . LIST_ID . '/interest-categories/' . INTEREST_TYPE . '/interests?count=100&output=json';
+		$interest_response = \Httpful\Request::get($interest_url)
+		                                     ->authenticateWith(API_KEY, API_KEY)        // authenticate with basic auth...
+		                                     ->send();
+
+		if(isset($interest_response->body) && isset($interest_response->body->interests) && is_array($interest_response->body->interests)) {
+			$interests = $interest_response->body->interests;
+			wp_cache_set('rg_interests', $interests, 'rg', 300);
+		} else {
+			// Something went very wrong here
+			error_log('RG: Problem grabbing the Interest Categories from Mailchimp!');
+			$interests = [];
+		}
+	}
+
+	return $interests;
+}
 
 // used to ONLY process the signups
 function rg_mailchimp_only_signup() {
@@ -416,7 +445,6 @@ function rg_mailchimp_only_signup() {
 }
 
 add_shortcode( 'rgmsignuponly' , 'rg_mailchimp_only_signup' );
-
 
 
 function rg_update_sub_link() {
